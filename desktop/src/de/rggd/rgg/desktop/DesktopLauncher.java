808969c99ee5e460f73fd9389a5a7911a9b50163
m2x4m4l42n <max.vonall@gmail.com>
2019-03-31 18:43:46 +0200

    Initial Project Setup

---
+++ desktop/src/de/rggd/rgg/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.rggd.rgg.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.rggd.rgg.RichtigGeilesGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RichtigGeilesGame(), config);
+	}
+}
