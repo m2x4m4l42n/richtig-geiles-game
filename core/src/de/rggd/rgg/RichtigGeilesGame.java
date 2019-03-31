ee0c631e3befd260b7c2cd54bc160c8d2eada7d0
m2x4m4l42n <max.vonall@gmail.com>
2019-03-31 19:04:39 +0200

    Added some Comments and an empty update method

--- core/src/de/rggd/rgg/RichtigGeilesGame.java
+++ core/src/de/rggd/rgg/RichtigGeilesGame.java
@@ -5,19 +5,31 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
+/*
+ * Main Game Class  
+ */
 public class RichtigGeilesGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 	}
-
+	/*
+	 * Updates gamestate before each render
+	 */
+	public void update() {
+		
+	}
+	
 	@Override
 	public void render () {
+		// Run update method before rendering
+		update();
+		
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
