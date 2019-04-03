64130a7a534820a9f55678f451b146fd515e4add
m2x4m4l42n <max.vonall@gmail.com>
2019-04-03 16:55:23 +0200

    migrated some code from old project

--- core/src/de/rggd/rgg/RichtigGeilesGame.java
+++ core/src/de/rggd/rgg/RichtigGeilesGame.java
@@ -3,26 +3,39 @@ package de.rggd.rgg;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+
+import de.rggd.rgg.character.CharacterRenderer;
+import de.rggd.rgg.map.MapRenderer;
 /*
  * Main Game Class  
  */
 public class RichtigGeilesGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+
+	MapRenderer renderer;
+	OrthographicCamera camera;
+	CharacterRenderer character;
 	
 	@Override
+	
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		renderer = new MapRenderer( new TmxMapLoader().load("maps/tmx/map.tmx"));
+		camera = new OrthographicCamera(Gdx.graphics.getHeight(),Gdx.graphics.getWidth());
+		camera.setToOrtho(false,Gdx.graphics.getWidth()/1.0f, Gdx.graphics.getHeight()/1.0f);
+		character = new CharacterRenderer("character/character.png");
+		character.create();
+		renderer.addCharacter(character);
+		
+		
 	}
 	/*
 	 * Updates gamestate before each render
 	 */
 	public void update() {
-		
+		camera.update();
+		character.update();
+		renderer.setView(camera);
 	}
 	
 	@Override
@@ -32,14 +45,12 @@ public class RichtigGeilesGame extends ApplicationAdapter {
 		
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		renderer.render();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		character.dispose();
+		renderer.dispose();
 	}
 }
