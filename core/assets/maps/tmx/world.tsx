cded9b48cadb6286fdd214d46fd887a44255ccf0
m2x4m4l42n <max.vonall@gmail.com>
2019-04-03 14:14:52 +0200

    Added some assets for a random first map and character

---
+++ core/assets/maps/tmx/world.tsx
@@ -0,0 +1,76 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="world" tilewidth="16" tileheight="16" tilecount="1440" columns="40">
+ <image source="../gfx/Overworld.png" width="640" height="576"/>
+ <tile id="86">
+  <objectgroup draworder="index">
+   <object id="1" x="4.90909" y="0.181818" width="10.7273" height="16.1818"/>
+  </objectgroup>
+ </tile>
+ <tile id="87">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.181818" y="0" width="16.5455" height="16.5455"/>
+  </objectgroup>
+ </tile>
+ <tile id="88">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.181818" y="-0.181818" width="16.5455" height="16.3636"/>
+  </objectgroup>
+ </tile>
+ <tile id="89">
+  <objectgroup draworder="index">
+   <object id="1" x="0.181818" y="-0.181818" width="15.6364" height="15.8182"/>
+  </objectgroup>
+ </tile>
+ <tile id="90">
+  <objectgroup draworder="index">
+   <object id="1" x="0.181818" y="0.363636" width="11.0909" height="15.4545"/>
+  </objectgroup>
+ </tile>
+ <tile id="126">
+  <objectgroup draworder="index">
+   <object id="1" x="2.36364" y="0.363636" width="13.6364" height="3.45455"/>
+   <object id="2" x="7.27273" y="0" width="8.54545" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="127">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="15.6364" height="16.1818"/>
+  </objectgroup>
+ </tile>
+ <tile id="128">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.181818" y="-0.181818" width="16.5455" height="8.18182"/>
+  </objectgroup>
+ </tile>
+ <tile id="129">
+  <objectgroup draworder="index">
+   <object id="1" x="0.181818" y="0" width="15.4545" height="15.8182"/>
+  </objectgroup>
+ </tile>
+ <tile id="130">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.363636" y="3.63636" width="9.27273" height="12.3636"/>
+   <object id="2" x="-0.545455" y="0.181818" width="13.6364" height="3.63636"/>
+  </objectgroup>
+ </tile>
+ <tile id="166">
+  <objectgroup draworder="index">
+   <object id="1" x="7.63636" y="-0.545455" width="8.72727" height="12.5455"/>
+  </objectgroup>
+ </tile>
+ <tile id="167">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.181818" y="0.363636" width="16.3636" height="11.4545"/>
+  </objectgroup>
+ </tile>
+ <tile id="169">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.181818" y="-0.545455" width="16.5455" height="12.9091"/>
+  </objectgroup>
+ </tile>
+ <tile id="170">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.727273" y="-0.727273" width="9.63636" height="14.1818"/>
+  </objectgroup>
+ </tile>
+</tileset>
