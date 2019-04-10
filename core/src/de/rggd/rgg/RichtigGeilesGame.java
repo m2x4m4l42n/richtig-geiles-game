a0655b3d64c58b3f63cc1293ba43603a786ea3ad
m2x4m4l42n <max.vonall@gmail.com>
2019-04-10 13:02:50 +0200

    Moved Startup code out of game class

--- core/src/de/rggd/rgg/RichtigGeilesGame.java
+++ core/src/de/rggd/rgg/RichtigGeilesGame.java
@@ -1,31 +1,25 @@
 package de.rggd.rgg;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-import de.rggd.rgg.character.CharacterRenderer;
-import de.rggd.rgg.map.MapRenderer;
+import de.rggd.rgg.screen.WorldScreen;
+
 /*
  * Main Game Class  
  */
-public class RichtigGeilesGame extends ApplicationAdapter {
+public class RichtigGeilesGame extends Game {
+	public static final int V_WIDTH = 600;
+	public static final int V_HEIGHT = 480;
+	public SpriteBatch batch;
 
-	MapRenderer renderer;
-	OrthographicCamera camera;
-	CharacterRenderer character;
 	
 	@Override
 	
 	public void create () {
-		renderer = new MapRenderer( new TmxMapLoader().load("maps/tmx/map.tmx"));
-		camera = new OrthographicCamera(Gdx.graphics.getHeight(),Gdx.graphics.getWidth());
-		camera.setToOrtho(false,Gdx.graphics.getWidth()/1.0f, Gdx.graphics.getHeight()/1.0f);
-		character = new CharacterRenderer("character/character.png");
-		character.create();
-		renderer.addCharacter(character);
+		batch = new SpriteBatch();
+		setScreen(new WorldScreen(this));
+		
 		
 		
 	}
@@ -33,24 +27,16 @@ public class RichtigGeilesGame extends ApplicationAdapter {
 	 * Updates gamestate before each render
 	 */
 	public void update() {
-		camera.update();
-		character.update();
-		renderer.setView(camera);
+		
 	}
 	
 	@Override
 	public void render () {
-		// Run update method before rendering
-		update();
-		
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		renderer.render();
+		super.render();
 	}
 	
 	@Override
 	public void dispose () {
-		character.dispose();
-		renderer.dispose();
+
 	}
 }
